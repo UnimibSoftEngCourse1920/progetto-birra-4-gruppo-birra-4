ad95d9d8bf23c04c1966dd368df5a97dafcb9d43
Andrea Brivio <a.brivio20@campus.unimib.it>
2020-01-23 15:09:52 +0100

    terza commit-inizializzazione

---
+++ src/main/java/GruppoBirra4/BrewDay/App.java
@@ -0,0 +1,13 @@
+package GruppoBirra4.BrewDay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
