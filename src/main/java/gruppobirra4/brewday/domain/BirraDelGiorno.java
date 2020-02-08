5aca3ec71103fd1f3a7801551eef4834d8ef87e6
Riccardo Maria Frighi <r.frighi@campus.unimib.it>
2020-02-08 13:56:02 +0100

    Test BirraDelGiorno

--- src/main/java/gruppobirra4/brewday/domain/BirraDelGiorno.java
+++ src/main/java/gruppobirra4/brewday/domain/BirraDelGiorno.java
@@ -2,6 +2,7 @@ package gruppobirra4.brewday.domain; //NOSONAR
 
 import java.util.Collections;
 import java.util.Map.Entry;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 import org.mapdb.HTreeMap;
@@ -10,53 +11,58 @@ import gruppobirra4.brewday.database.Database;
 import gruppobirra4.brewday.domain.ingredienti.CatalogoIngredienti;
 import gruppobirra4.brewday.domain.ingredienti.Ingrediente;
 import gruppobirra4.brewday.domain.ricette.Ricetta;
+import gruppobirra4.brewday.domain.ricette.Ricettario;
 import gruppobirra4.brewday.errori.Notifica;
 
 public class BirraDelGiorno {
 	
-	private TreeMap<String, Double> valori;
+	private static TreeMap<String, Double> valori = new TreeMap<>();
 	
 
 	private BirraDelGiorno() {
 		super();
 	}
 	
-	private HTreeMap<String, Ricetta> openMapDB() {
-		return (HTreeMap<String, Ricetta>) Database.getIstanza().openMapDB("Ricettario");
+	private static HTreeMap<String, Ricetta> openMapDB() {
+		return (HTreeMap<String, Ricetta>) Database.getIstanza().openMapDB("CatalogoIngredienti");
 	}
 	
-	public Ricetta calcolaBirraDelGiorno(String quantitaBirra) {
+	public static Ricetta calcolaBirraDelGiorno(String quantitaBirra) {
 		if(!(Ricetta.validateQuantitaBirra(quantitaBirra))) {
 			return null;
 		}
-		HTreeMap<String, Ricetta> ricette = openMapDB();
+		SortedMap<String, Ricetta> ricette = Ricettario.getIstanza().getRicette();
 		if(ricette.isEmpty()) {
 			Database.getIstanza().closeDB();
 			Notifica.getIstanza().addError("Non ci sono ricette nel ricettario");
 			return null;
 		}
+		SortedMap<String, Ingrediente> catalogo = CatalogoIngredienti.getIstanza().getIngredienti();
 		for (Ricetta r: ricette.values()) {
-			valori.put(r.getId(), calcolaDifferenza(r, Double.parseDouble(quantitaBirra)));
+			valori.put(r.getId(), calcolaDifferenza(catalogo, r, Double.parseDouble(quantitaBirra)));
 		}
 		return ricette.get(trovaValoreMax(valori));
 		
 		
 	}
 
-	private Double calcolaDifferenza(Ricetta ricetta, double quantitaBirra) {
+	private static Double calcolaDifferenza(SortedMap<String, Ingrediente> catalogo, Ricetta ricetta, double quantitaBirra) {
 		double differenza = 0;
 		for (Ingrediente ingRicetta : ricetta.getIngredienti()) {
-			String id = ingRicetta.getId();
-			double quantitaCatalogo = CatalogoIngredienti.getIstanza().getIngredienteById(id).getQuantita();
+			Ingrediente ingCatalogo = getIngredienteByNomeCategoria(catalogo, ingRicetta.getNome(), ingRicetta.getCategoria());
+			if(ingCatalogo == null) {
+				return Double.MAX_VALUE;
+			}
+			double quantitaCatalogo = ingCatalogo.getQuantita();
 			double quantitaRicetta = ingRicetta.getQuantita() * quantitaBirra;
 			if(quantitaCatalogo - (quantitaRicetta)<0)
 				return Double.MAX_VALUE;
-			differenza = quantitaCatalogo - quantitaRicetta;
+			differenza = differenza +(quantitaCatalogo - quantitaRicetta);
 		}
 		return differenza;
 	}
 	
-	private String trovaValoreMax(TreeMap<String, Double> valori) {
+	private static String trovaValoreMax(TreeMap<String, Double> valori) {
 		Double max = Collections.max(valori.values());
 		String key = null;
 		for(Entry<String, Double> entry : valori.entrySet()) {
@@ -66,6 +72,15 @@ public class BirraDelGiorno {
 			}
 		}
 		return key;
+	}
+	
+	public static Ingrediente getIngredienteByNomeCategoria(SortedMap<String, Ingrediente> catalogo, String nome, String categoria) {
+		for (Ingrediente ing : catalogo.values()) {
+			if((ing.getNome().equals(nome)) && (ing.getCategoria().equals(categoria))) {
+				return ing;
+			}
+		}
+		return null;
 	}
 	
 }
---
+++ src/test/java/gruppobirra4/brewday/BirraDelGiornoTest.java
@@ -0,0 +1,78 @@
+package gruppobirra4.brewday;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import gruppobirra4.brewday.domain.BirraDelGiorno;
+import gruppobirra4.brewday.domain.ingredienti.CatalogoIngredienti;
+import gruppobirra4.brewday.domain.ingredienti.Ingrediente;
+import gruppobirra4.brewday.domain.ricette.Ricetta;
+import gruppobirra4.brewday.domain.ricette.Ricettario;
+
+public class BirraDelGiornoTest {
+
+	@Test
+	public void testCalcolaBirraDelGiorno() {
+		CatalogoIngredienti c = CatalogoIngredienti.getIstanza();
+		Ricettario r = Ricettario.getIstanza();
+		
+		Set<Ingrediente> ingredientiRicetta1 = new HashSet<>();
+		Ingrediente ing1 = Ingrediente.creaIngrediente(null, "Pale", "MALTO", "3800");
+		ingredientiRicetta1.add(ing1);
+		Ingrediente ing2 = Ingrediente.creaIngrediente(null, "Crystal", "MALTO", "200");
+		ingredientiRicetta1.add(ing2);
+		Ingrediente ing3 = Ingrediente.creaIngrediente(null, "Columbus", "LUPPOLO", "45");
+		ingredientiRicetta1.add(ing3);
+		Ricetta ricetta1 = r.creaRicetta("American Pale Ale", "bla bla bla", ingredientiRicetta1, "17", "18");
+		assertNotNull(ricetta1);
+		
+		Set<Ingrediente> ingredientiRicetta2 = new HashSet<>();
+		Ingrediente ing4 = Ingrediente.creaIngrediente(null, "Pils", "MALTO", "2100");
+		ingredientiRicetta2.add(ing4);
+		Ingrediente ing5 = Ingrediente.creaIngrediente(null, "Wheat", "MALTO", "2100");
+		ingredientiRicetta2.add(ing5);
+		Ingrediente ing6 = Ingrediente.creaIngrediente(null, "Hattertauer", "LUPPOLO", "17");
+		ingredientiRicetta2.add(ing6);
+		Ricetta ricetta2 = r.creaRicetta("Hefe Weinzen", "blu blu blu", ingredientiRicetta2, "15", "18");
+		assertNotNull(ricetta2);
+		
+		Set<Ingrediente> ingredientiRicetta3 = new HashSet<>();
+		Ingrediente ing7 = Ingrediente.creaIngrediente(null, "Pils", "MALTO", "2900");
+		ingredientiRicetta3.add(ing7);
+		Ingrediente ing8 = Ingrediente.creaIngrediente(null, "Wheat", "MALTO", "3700");
+		ingredientiRicetta3.add(ing8);
+		Ingrediente ing9 = Ingrediente.creaIngrediente(null, "Hattertauer", "LUPPOLO", "51"); //più della quantita presente
+		ingredientiRicetta3.add(ing9);
+		Ricetta ricetta3 = r.creaRicetta("Mille bolle", "ble ble ble", ingredientiRicetta2, "13", "18");
+		assertNotNull(ricetta3);
+		
+		Set<Ingrediente> ingredientiRicetta4 = new HashSet<>();
+		Ingrediente ing10 = Ingrediente.creaIngrediente(null, "Pale", "MALTO", "3800");
+		ingredientiRicetta1.add(ing10);
+		Ingrediente ing11 = Ingrediente.creaIngrediente(null, "Crystal", "MALTO", "200");
+		ingredientiRicetta1.add(ing11);
+		Ingrediente ing12 = Ingrediente.creaIngrediente(null, "Astrubale", "LUPPOLO", "45"); //non presente in catalogo
+		ingredientiRicetta1.add(ing12);
+		Ricetta ricetta4 = r.creaRicetta("Zaraba", "blo blo blo", ingredientiRicetta1, "17", "18");
+		assertNotNull(ricetta4);
+		
+		
+		c.creaIngrediente("Pale", "MALTO", "5000");
+		c.creaIngrediente("Crystal", "MALTO", "1000");
+		c.creaIngrediente("Columbus", "LUPPOLO", "500");
+		c.creaIngrediente("Pils", "MALTO", "3000");
+		c.creaIngrediente("Wheat", "MALTO", "4500");
+		c.creaIngrediente("Hattertauer", "LUPPOLO", "50");
+		
+		Ricetta risultato = BirraDelGiorno.calcolaBirraDelGiorno("18");
+		assertNotNull(risultato);
+		assertEquals(ricetta1.getId(), risultato.getId());
+		
+	}
+
+}
